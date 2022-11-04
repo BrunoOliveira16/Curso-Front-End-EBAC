ad36468a81f935e1563c3a1dc4e13cdb6b8c482b
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2022-11-03 22:25:11 -0300

    Etapa 02 codeTogether - css body, search e input

---
+++ Modulo-28-Ajax/Code-Together_M28/js/script.js
@@ -0,0 +1,29 @@
+const url = 'https://api.github.com/users';
+const main = document.getElementById('main')
+let text = ''
+
+
+function getUser(user){
+
+    fetch(`${url}/${user}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        }
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            text += `
+            <div>
+                <h3>${data.name} possui ${data.public_repos} repositórios públicos no GitHub</h3>
+                <img src="${data.avatar_url}" alt="avatar">
+            </div><br/>`
+            main.innerHTML = text
+    })
+    .catch((error) => console.error('Erro: ', error.message || error))
+}
+
+const setUser = document.getElementById('username')
+
+
+
