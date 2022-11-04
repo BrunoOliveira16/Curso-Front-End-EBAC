feaf5c49f51b23ca11e78905327233ae179330df
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2022-11-04 20:12:29 -0300

    Finalizando CodeTogether - Onepage que realiza chamada assincrono para a API do GitHub

--- Modulo-28-Ajax/Code-Together_M28/js/script.js
+++ Modulo-28-Ajax/Code-Together_M28/js/script.js
@@ -1,37 +1,55 @@
 const url = 'https://api.github.com/users';
-const main = document.getElementById('main')
-let text = ''
+const main = document.getElementById('main');
+const btnSubmit = document.getElementById('search');
+const input = document.getElementById('username');
+let text = '';
 
+// Função para buscar dados da API do GitHub
+btnSubmit.addEventListener('click', (user) => {
+    if (input.value !== ""){
+       user = (input.value)
+    }
 
-function getUser(user){
+    fetch(`${url}/${user}`)
+    .then((response) => response.json())
+    .then((data) => {
+        text += `
+        <div class="container-user">
+            <div class="box-img">
+                <img src="${data.avatar_url}" alt="avatar">
+            </div>
 
-    fetch(`${url}/${user}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json; charset=UTF-8'
-        }
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            text += `
-            <div class="container-user">
-                <div class="box-img">
-                    <img src="${data.avatar_url}" alt="avatar">
-                </div>
-                <div class="box-info">
+            <div class="container-info">
+                <div class="box-name">
                     <h1>${data.name}</h1>
-                    <h3>Repositórios:${data.public_repos}</h3> 
+                    <p>${data.html_url}</p>
                 </div>
-            </div>`
-            main.innerHTML = text
+                <div class="container-data">
+                    <div class="box-info">
+                        <h3>Repos</h3>
+                        <h2>${data.public_repos}</h2>   
+                    </div>
+                    <div class="box-info">
+                        <h3>Followers</h3> 
+                        <h2>${data.followers}</h2>   
+                    </div>
+                    <div class="box-info">
+                        <h3>Following</h3>
+                        <h2>${data.following}</h2>   
+                    </div>      
+                </div>
+                <div class="box-subinfo">
+                    <h3>${data.location}</h3>
+                    <h3>${data.company}</h3>
+                </div>
+            </div>    
+        </div>`
+        main.innerHTML = text
     })
     .catch((error) => console.error('Erro: ', error.message || error))
-}
+});
 
-const setUser = document.getElementById('username')
-
-setUser.addEventListener('click', function userSubmit(event){
-    getUser(event.target.value)
-    console.log(setUser)
+// Limpar campo de busca
+btnSubmit.addEventListener('click', () => {
+    (document.getElementById('username').value = "")
 })
-
---
+++ Modulo-28-Ajax/Code-Together_M28/readme.md
@@ -0,0 +1,8 @@
+## 📚 CodeTogether Módulo 28 - Desenvolvedor Full Stack Python
+
+**Tarefa:** Criar uma onepage que faça chamadas assincronas para uma API pública (ex:GitHub) e adicionar CSS na página para deixar uma aparência mais interessante para o usuário.
+
+Endereço API GitHub: 
+- [GitHub > Users](https://docs.github.com/en/rest/reference/users)
+
+Link para o Navegador:
