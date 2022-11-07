7b920f5d78749aabc3cb3b78f9d42a4cc5c6bc75
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2022-11-07 00:34:56 -0300

    Etapa 04 concluida - Criado funções de validação, consumo api correios, limpar formulario e evento para atualizar o endereço de forma automatica

--- Modulo-29-Ajax-II/assets/js/script.js
+++ Modulo-29-Ajax-II/assets/js/script.js
@@ -10,10 +10,13 @@ const uf = document.getElementById("uf");
 const mensagem = document.querySelector("#mensagem");
 const notNull = document.getElementsByClassName("not-null");
 
+
+// Função de verificação de campos em branco
 function isEmpty(elem){
     return elem.value.length < 1 ? `O campo <strong>${elem.name}</strong> não pode ser vazio.` : "" 
-}
+};
 
+// Função para envio do formulário
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -26,11 +29,65 @@ form.addEventListener("submit", (event) => {
             msg.push(fieldState);
     })
 
+    // Validação email
+    const isEmail = validaEmail(email);
+    if(isEmail) msg.push(isEmail);
+
+    // Validação CEP
+    const isCEP = validaCEP(cep);
+    if(isCEP.length > 0){
+        msg.push(isCEP);
+    }
+
     msg.forEach(item => {
         markup += `<p>${item}</p>`
     });
 
     mensagem.innerHTML = markup;
+});
+
+
+// Funções de Validação dos campos
+function validaEmail(elem){
+    return elem.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ? "" : `Digite um <strong>e-mail</strong>válido`;
+};
+
+function validaCEP(elem){
+    if (!elem.value.match(/^[0-9]{8}/))
+        return `Digite um CEP válido.`;
+    else return "";
+};
+
+// Consumo de API correios para aquisição de dados
+cep.addEventListener('focusout',()=>{
+    
+    const script = document.createElement("script");
+    script.src = "https://viacep.com.br/ws/" + cep.value + "/json?callback=updateAdress";
+    document.body.appendChild(script);
+
+});
+
+function updateAdress(data) {
+    if( !('erro' in data)) {
+        rua.value=(data.logradouro);
+        bairro.value=(data.bairro);
+        cidade.value=(data.localidade);
+        uf.value=(data.uf);
+        mensagem.innerHTML = '';
+    } else {
+        mensagem.innerHTML = `CEP não encontrado`;
+    }
+}
+
+
+// Limpar campos do formulário
+form.addEventListener('submit', () => {
+    nome.value = ""
+    email.value = ""
+    cpf.value = ""
+    cep.value = ""
+    rua.value = ""
+    cidade.value = ""
+    bairro.value = ""
+    uf.value = ""
 })
-
-
