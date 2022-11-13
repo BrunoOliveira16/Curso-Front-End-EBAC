27d65979c846c51f863fc4b20a1c756a2a8bd0b5
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2022-11-12 21:43:23 -0300

    Atualização exercicio modulo 25 - arrow function

--- Modulo-25-JavaScript-Advanced/CodeTogether-M25/assets/js/index.js
+++ Modulo-25-JavaScript-Advanced/CodeTogether-M25/assets/js/index.js
@@ -1,5 +1,8 @@
-//Modificando function com a utilização de arrow functions
-const obterMedia = () => {
+const btn = document.getElementById("btn");
+const btn01 = document.getElementById("btn-01");
+
+//Modificando função com a utilização de arrow functions
+const obterMedia = ()=> {
     // captura dos valores dos inputs para as variaveis abaixo
     let a = parseFloat(document.getElementById("a").value);
     let b = parseFloat(document.getElementById("b").value);
@@ -11,4 +14,36 @@ const obterMedia = () => {
     document.getElementById("resultado").innerHTML = "Resultado: " + media;
 };
 
+// Escutador de evento para limpar os campos após o submit
+btn.addEventListener('click', (event)=> {
+    event.preventDefault();
 
+    document.getElementById("a").value = "";
+    document.getElementById("b").value = "";
+    document.getElementById("c").value = "";
+});
+
+
+const CalcularMedia = () => {
+    // const para calcular a media + método para reduzir casas decimais(toFixed)
+    let Nota01 = parseFloat(document.getElementById("N1").value);
+    let Nota02 = parseFloat(document.getElementById("N2").value);
+    let Nota03 = parseFloat(document.getElementById("N3").value);
+    let Nota04 = parseFloat(document.getElementById("N4").value);
+    const MediaNota = ((Nota01 + Nota02 + Nota03 + Nota04)/4).toFixed(2);
+
+    // Condicional if/else para aprovado ou reprovado
+    MediaNota.value >= 7 ? resultado = "Aprovado" : resultado = "Reprovado";
+
+    document.getElementById("exibicao").innerHTML = "Media: " + MediaNota + " - " + resultado;
+};
+
+// Escutador de evento para limpar os campos após o submit
+btn01.addEventListener('click', e => {
+    e.preventDefault();
+
+    document.getElementById("N1").value = "";
+    document.getElementById("N2").value = "";
+    document.getElementById("N3").value = "";
+    document.getElementById("N4").value = "";
+})
