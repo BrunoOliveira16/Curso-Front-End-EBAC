39acb207b2d3d1722452dd0aff957db5555705b7
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2022-11-19 16:24:55 -0300

    Atualização Modulo 33 - Code Together

---
+++ Modulo-33-Build-and-Delivery/Code-Together-M33/style-project/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import * as React from 'react';
+import Dashboard from './components/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
