6bbfff57953c4668eb36e9cb5c71abcdeb99ea82
Bruno Oliveira <103857382+BrunoOliveira16@users.noreply.github.com>
2022-11-20 20:30:39 -0300

    Feat: Atualização de informações no readme, referente ao modulo 33

--- Modulo-33-Build-and-Delivery/Code-Together-M33/style-project/src/App.test.js
+++ Modulo-33-Build-and-Delivery/Code-Together-M33/style-project/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('teste titulo', () => {
   render(<App />);
+  
   // eslint-disable-next-line testing-library/no-node-access
   const testCase = document.querySelector('.App h1');
   expect(testCase).toHaveTextContent(/Meu Painel de Controle/i);
